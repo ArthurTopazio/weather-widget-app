391cdf1807f3178bc65b2107b440db30d2211bdf
ArthurTopazio <perereacmb@gmail.com>
2022-09-01 14:42:49 +0300

    add console.warn

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import WeatherWidget from './components/WeatherWidget/WeatherWidget';
 
 const App: React.FC = () => {
 
+  console.warn('menu like burg, locale storage, visual upd');
+
   return (
     <>
       <WeatherWidget />
